3ec939c68eba2abf4748cb2991b4179a7e147a16
julierealmids <jullietnakayizaa@gmail.com>
2022-03-02 08:22:39 +0300

    some changes

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun main() {
+    var text = "akirachix"
+    println(text[0])
+    println(text[3])
+    println(text[4])
+    var greeting = "hi, my name is "
+    var name = "Julliet i am "
+    var age = "22 year old"
+    var stmt = "$greeting ,$name $age"
+    println(stmt)
+    var myname = "julliet"
+    var o = myname
+    println(o.length)
+    var name1 = "nakayiza"
+    var name2 = "namusoke"
+    if(name1==name2)
+    println("that's me")
+    else (
+    println("i dont know who that is"))
+}
+
+
+
+
